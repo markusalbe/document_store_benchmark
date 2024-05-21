ab094800802506d8072d9d2e7045111eaf90035c
Marcos Albe <markus.albe@gmail.com>
2024-05-21 08:20:06 -0400

    more symlinks corrections

--- benchmark.data.sh
+++ benchmark.data.sh
@@ -137,7 +137,7 @@ function extract_and_split_lookup_conditions_using_symlinks() {
     } done
 
     # symlink mongodb as xcom for PK conditions
-    ln -vfs "$(get_lookup_chunks_datadir 'mongodb' 'pk_lookup')/" "$(get_lookup_chunks_datadir  'xcom' 'pk_lookup')/";
+    ln -vfs "$(get_lookup_chunks_datadir 'mongodb' 'pk_lookup')" "$(get_lookup_chunks_datadir  'xcom' 'pk_lookup')";
 
     # symlink mysql as mongodb and xcom for SK conditions
     ln -vfs "$(get_lookup_chunks_datadir 'mysql' 'sk_lookup')" "$(get_lookup_chunks_datadir 'xcom' 'sk_lookup')";
