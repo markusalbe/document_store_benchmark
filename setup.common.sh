0a8f0bc8e1fa56259b8c77de2939eda8907ee53c
Marcos Albe <markus.albe@gmail.com>
2024-05-21 06:33:28 -0400

    add jq

--- setup.common.sh
+++ setup.common.sh
@@ -6,7 +6,7 @@ function fix_yum_repos() {
 
 function install_linux_tooling() {
     yes | yum install -y epel-release sudo;
-    yes | yum install -y which less tar git wget vim screen strace gdb perf bc python3 python3-devel python3-pip libaio bash-completion nc gcc-c++ cmake fuse fuse-devel net-tools sysstat psmisc;
+    yes | yum install -y which less tar git wget vim screen strace gdb perf bc jq python3 python3-devel python3-pip libaio bash-completion nc gcc-c++ cmake fuse fuse-devel net-tools sysstat psmisc;
     rpm -Uvh http://mirror.centos.org/centos/8-stream/AppStream/x86_64/os/Packages/jq-1.6-3.el8.x86_64.rpm;
 }
 
